6c8c42eeaaee79972af6fbb918e6d1febc940bbf
Hammed Oyedele <itz.harmid@gmail.com>
2023-01-19 15:39:03 +0100

    docs: add the counter example

--- src/index.js
+++ src/index.js
@@ -8,12 +8,10 @@ function MyApp(props) {
   return (
     <gtk-window defaultHeight={600} defaultWidth={800} title='My App'>
       <gtk-box orientation={Gtk.Orientation.VERTICAL} spacing={50}>
-        {count >= 5 && <gtk-label label={`${count}`} />}
+        <gtk-label label={`${count}`} />
+
         <gtk-button onClicked={() => setCount(count + 1)} label='Click Me'>
-          <gtk-box spacing={50}>
-            {count < 5 && <gtk-label label={`${count}`} />}
-            <gtk-label label='Click me MF' />
-          </gtk-box>
+          <gtk-label label='Click me MF' />
         </gtk-button>
       </gtk-box>
     </gtk-window>
