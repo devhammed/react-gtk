1bbd89ca1ae984ce7c5dfe93d38be94cea6559c4
Hammed Oyedele <itz.harmid@gmail.com>
2023-01-20 11:29:36 +0100

    feat: add the multiple windows support

--- src/index.js
+++ src/index.js
@@ -9,21 +9,38 @@ import {
 import { useEffect, useMemo, useRef, useState } from 'react';
 
 function MyApp(props) {
+  const secondWinRef = useRef(null);
   const [count, setCount] = useState(0);
   const hasClickedFiveTimes = useMemo(() => count === 5, [count]);
 
   return (
-    <GtkWindow defaultHeight={600} defaultWidth={800} title='My App'>
-      <GtkBox orientation={GtkOrientation.VERTICAL} spacing={50}>
-        <GtkLabel label={count.toString()} />
-        <GtkButton
-          label={hasClickedFiveTimes ? 'You are now a developer!' : 'Click Me'}
-          onClicked={
-            hasClickedFiveTimes ? null : () => setCount((count) => count + 1)
-          }
-        />
-      </GtkBox>
-    </GtkWindow>
+    <>
+      <GtkWindow defaultHeight={600} defaultWidth={800} title='My App'>
+        <GtkBox orientation={GtkOrientation.VERTICAL} spacing={50}>
+          <GtkLabel label={count.toString()} />
+          <GtkButton
+            label={
+              hasClickedFiveTimes
+                ? 'You are now a developer, click again!'
+                : 'Click Me'
+            }
+            onClicked={
+              hasClickedFiveTimes
+                ? () => secondWinRef.current?.present()
+                : () => setCount((count) => count + 1)
+            }
+          />
+        </GtkBox>
+      </GtkWindow>
+
+      <GtkWindow
+        ref={secondWinRef}
+        defaultHeight={600}
+        defaultWidth={800}
+        title='Royal Hotness'>
+        <GtkLabel label='I am here to tell you that your head is hot.' />
+      </GtkWindow>
+    </>
   );
 }
 
