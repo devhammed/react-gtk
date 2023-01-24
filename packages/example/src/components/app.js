ed9eb7566dfb19b34751e3d411d22177e21c58f4
Hammed Oyedele <itz.harmid@gmail.com>
2023-01-24 01:26:58 +0100

    build: add meson building support

---
+++ packages/example/src/components/app.js
@@ -0,0 +1,122 @@
+import {
+  GtkBox,
+  GtkButton,
+  GtkEntry,
+  GtkLabel,
+  GtkStack,
+  GtkStackPage,
+  GtkTextView,
+  GtkWindow,
+} from 'react-gtk-renderer';
+import { useEffect, useMemo, useRef, useState } from 'react';
+
+const { Gtk } = imports.gi;
+
+export function MyApp(props) {
+  const stackRef = useRef(null);
+  const firstPageRef = useRef(null);
+  const secondPageRef = useRef(null);
+  const secondWinRef = useRef(null);
+  const [count, setCount] = useState(0);
+  const hasClickedSixTimes = useMemo(() => count === 6, [count]);
+
+  return (
+    <>
+      <GtkWindow defaultHeight={600} defaultWidth={800} title='My App'>
+        <GtkStack
+          ref={stackRef}
+          transitionType={Gtk.StackTransitionType.SLIDE_RIGHT}>
+          <GtkStackPage
+            ref={firstPageRef}
+            marginStart={25}
+            marginEnd={25}
+            spacing={25}
+            valign={Gtk.Align.CENTER}
+            halign={Gtk.Align.CENTER}
+            orientation={Gtk.Orientation.VERTICAL}>
+            <GtkLabel label='Hello World' />
+
+            {hasClickedSixTimes &&
+              '<i>Hi World, testing insertBefore and text instance!</i>'}
+
+            <GtkEntry
+              onChanged={(entry) => {
+                console.log(entry.buffer.text);
+              }}
+            />
+
+            <GtkButton
+              onClicked={
+                hasClickedSixTimes
+                  ? () => {
+                      secondWinRef.current?.present();
+                      setCount(0);
+                    }
+                  : () => setCount((prev) => prev + 1)
+              }>
+              {hasClickedSixTimes
+                ? `You are now a developer, click again!`
+                : `You have clicked me ${count} times`}
+            </GtkButton>
+
+            <GtkButton
+              label='Next page'
+              onClicked={() => {
+                const stack = stackRef.current;
+                const secondPage = secondPageRef.current;
+
+                if (stack && secondPage) {
+                  stack.set_visible_child(secondPage);
+                }
+              }}
+            />
+          </GtkStackPage>
+
+          <GtkStackPage
+            ref={secondPageRef}
+            marginStart={25}
+            marginEnd={25}
+            spacing={25}
+            valign={Gtk.Align.CENTER}
+            halign={Gtk.Align.CENTER}
+            orientation={Gtk.Orientation.VERTICAL}>
+            <GtkLabel label='Hi World' />
+
+            <GtkButton
+              label='Previous page'
+              onClicked={() => {
+                const stack = stackRef.current;
+                const firstPage = firstPageRef.current;
+
+                if (stack && firstPage) {
+                  stack.set_visible_child(firstPage);
+                }
+              }}
+            />
+          </GtkStackPage>
+        </GtkStack>
+      </GtkWindow>
+
+      <GtkWindow
+        modal
+        ref={secondWinRef}
+        defaultHeight={500}
+        defaultWidth={500}
+        title='Royal Hotness'>
+        <GtkBox
+          marginStart={25}
+          marginEnd={25}
+          spacing={25}
+          valign={Gtk.Align.CENTER}
+          halign={Gtk.Align.CENTER}
+          orientation={Gtk.Orientation.VERTICAL}>
+          <GtkLabel label='I am here to tell you that your head is hot.' />
+
+          <GtkButton onClicked={() => secondWinRef.current?.close()}>
+            Close Me
+          </GtkButton>
+        </GtkBox>
+      </GtkWindow>
+    </>
+  );
+}
--- packages/example/src/index.js
+++ packages/example/src/index.js
@@ -1,129 +1,19 @@
-import {
-  createRoot,
-  GtkBox,
-  GtkButton,
-  GtkEntry,
-  GtkLabel,
-  GtkStack,
-  GtkStackPage,
-  GtkTextView,
-  GtkWindow,
-} from 'react-gtk-renderer';
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { MyApp } from './components/app';
+import { createRoot } from 'react-gtk-renderer';
 
-const { Gtk } = ((imports.gi.versions.Gtk = '4.0'), imports.gi);
-
-function MyApp(props) {
-  const stackRef = useRef(null);
-  const firstPageRef = useRef(null);
-  const secondPageRef = useRef(null);
-  const secondWinRef = useRef(null);
-  const [count, setCount] = useState(0);
-  const hasClickedSixTimes = useMemo(() => count === 6, [count]);
-
-  return (
-    <>
-      <GtkWindow defaultHeight={600} defaultWidth={800} title='My App'>
-        <GtkStack
-          ref={stackRef}
-          transitionType={Gtk.StackTransitionType.SLIDE_RIGHT}>
-          <GtkStackPage
-            ref={firstPageRef}
-            marginStart={25}
-            marginEnd={25}
-            spacing={25}
-            valign={Gtk.Align.CENTER}
-            halign={Gtk.Align.CENTER}
-            orientation={Gtk.Orientation.VERTICAL}>
-            <GtkLabel label='Hello World' />
-
-            {hasClickedSixTimes &&
-              '<i>Hi World, testing insertBefore and text instance!</i>'}
-
-            <GtkEntry
-              onChanged={(entry) => {
-                console.log(entry.buffer.text);
-              }}
-            />
-
-            <GtkButton
-              onClicked={
-                hasClickedSixTimes
-                  ? () => {
-                      secondWinRef.current?.present();
-                      setCount(0);
-                    }
-                  : () => setCount((prev) => prev + 1)
-              }>
-              {hasClickedSixTimes
-                ? `You are now a developer, click again!`
-                : `You have clicked me ${count} times`}
-            </GtkButton>
-
-            <GtkButton
-              label='Next page'
-              onClicked={() => {
-                const stack = stackRef.current;
-                const secondPage = secondPageRef.current;
-
-                if (stack && secondPage) {
-                  stack.set_visible_child(secondPage);
-                }
-              }}
-            />
-          </GtkStackPage>
-
-          <GtkStackPage
-            ref={secondPageRef}
-            marginStart={25}
-            marginEnd={25}
-            spacing={25}
-            valign={Gtk.Align.CENTER}
-            halign={Gtk.Align.CENTER}
-            orientation={Gtk.Orientation.VERTICAL}>
-            <GtkLabel label='Hi World' />
-
-            <GtkButton
-              label='Previous page'
-              onClicked={() => {
-                const stack = stackRef.current;
-                const firstPage = firstPageRef.current;
-
-                if (stack && firstPage) {
-                  stack.set_visible_child(firstPage);
-                }
-              }}
-            />
-          </GtkStackPage>
-        </GtkStack>
-      </GtkWindow>
-
-      <GtkWindow
-        modal
-        ref={secondWinRef}
-        defaultHeight={500}
-        defaultWidth={500}
-        title='Royal Hotness'>
-        <GtkBox
-          marginStart={25}
-          marginEnd={25}
-          spacing={25}
-          valign={Gtk.Align.CENTER}
-          halign={Gtk.Align.CENTER}
-          orientation={Gtk.Orientation.VERTICAL}>
-          <GtkLabel label='I am here to tell you that your head is hot.' />
-
-          <GtkButton onClicked={() => secondWinRef.current?.close()}>
-            Close Me
-          </GtkButton>
-        </GtkBox>
-      </GtkWindow>
-    </>
-  );
-}
-
-const root = createRoot({
-  id: 'com.github.devhammed.reactGTK',
+imports.package.init({
+  name: '@PACKAGE_NAME@',
+  version: '@PACKAGE_VERSION@',
+  prefix: '@PREFIX@',
+  libdir: '@LIBDIR@',
 });
 
-root.render(<MyApp />);
+imports.package.run({
+  main: function (argv) {
+    const root = createRoot({
+      id: '@PACKAGE_NAME@',
+    });
+
+    root.render(<MyApp />, argv);
+  },
+});
