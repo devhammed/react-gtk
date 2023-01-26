1a178b2570f06b5a19ff30dd51a56359fa4c49db
Hammed Oyedele <itz.harmid@gmail.com>
2023-01-26 14:03:17 +0100

    build: migrate to gjs embed in c

--- examples/ts/src/index.tsx
+++ examples/ts/src/index.tsx
@@ -4,16 +4,17 @@ import { createRoot } from 'react-gtk-renderer';
 declare const imports: any;
 
 imports.package.init({
-  name: '@PACKAGE_NAME@',
+  name: '@PACKAGE_ID@',
   version: '@PACKAGE_VERSION@',
   prefix: '@PREFIX@',
   libdir: '@LIBDIR@',
+  pkgdatadir: '@PKGDATADIR@',
 });
 
 imports.package.run({
   main: function (argv: string[]) {
     const root = createRoot({
-      id: '@PACKAGE_NAME@',
+      id: '@PACKAGE_ID@',
     });
 
     root.render(<MyApp />, argv);
